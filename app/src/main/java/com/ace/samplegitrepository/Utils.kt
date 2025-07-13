4062907b06ebe499af03cd5375acf4c17ec65149
Iravani <saivn1990@gmail.com>
2025-07-13 10:07:58 +0330

    Utils.kt added to project

---
+++ app/src/main/java/com/ace/samplegitrepository/Utils.kt
@@ -0,0 +1,34 @@
+package com.ace.sampleandroidstudio
+
+class Utils {
+
+    companion object{
+
+        fun sayHi(name:String): String {
+            return "Hi, $name"
+        }
+        fun sayHello(name:String): String {
+            return "Hello, $name !"
+        }
+        fun printHello(name:String){
+            print(sayHello(name))
+        }
+
+        fun sayGoodBye(name:String): String {
+            return "GoodBye, $name"
+        }
+        fun printHi(name:String){
+            print(sayHi(name))
+            print("")
+            print("")
+            print("")
+            print("")
+            print("")
+            print("")
+            print(sayHi(name))
+            print(sayHi(name))
+            print(sayHi(name))
+        }
+    }
+
+}
