ca396042af5c3af30941134e0d4bcf6e29061d84
Iravani <saivn1990@gmail.com>
2025-07-14 11:20:15 +0330

    Utils2.kt added

---
+++ app/src/main/java/com/ace/samplegitrepository/Utils2.kt
@@ -0,0 +1,28 @@
+package com.ace.sampleandroidstudio
+
+class Utils2 {
+
+    companion object{
+
+
+        fun printHello(name:String){
+            print(name)
+        }
+
+
+        fun printAlaki(){
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+            printHello("alakiiiiiiiiiiiiiiiiiiiii")
+        }
+    }
+
+}
